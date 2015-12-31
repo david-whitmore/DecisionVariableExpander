4f980d67bae529663ff40c44709dc25c7e8b82d3
David Whitmore <whitters1793@googlemail.com>
2015-12-31 13:45:39 +0000

    Simplifying using Guice dependency injection.

--- src/main/java/org/dw/decision_variable_expander/Main.java
+++ src/main/java/org/dw/decision_variable_expander/Main.java
@@ -1,10 +1,33 @@
 package org.dw.decision_variable_expander;
 
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import com.csvreader.CsvWriter;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+
 public class Main {
-    private ConfigReader reader;
+    private final Provider<ConfigReader> readerFactory;
+    private final Provider<CsvWriter> writerFactory;
 
-    public static void main(String[] args) {
-
+    public Main(Provider<ConfigReader> readerFactory, Provider<CsvWriter> writerFactory) {
+        this.readerFactory = readerFactory;
+        this.writerFactory = writerFactory;
     }
 
+    private void outputAsCsv() throws JAXBException, IOException {
+        DecisionTable decisionTable = readerFactory.get().read(System.in);
+
+        decisionTable.outputAsCsv(writerFactory.get());
+    }
+
+    public static void main(String[] args) throws JAXBException, IOException {
+        Injector injector = Guice.createInjector(new Module());
+        Main main = injector.getInstance(Main.class);
+
+        main.outputAsCsv();
+    }
 }
---
+++ src/main/java/org/dw/decision_variable_expander/Module.java
@@ -0,0 +1,13 @@
+package org.dw.decision_variable_expander;
+
+import com.google.inject.AbstractModule;
+
+class Module extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
