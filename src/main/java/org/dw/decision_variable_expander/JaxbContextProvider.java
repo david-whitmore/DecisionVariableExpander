9a0e9d63ae26ab38ee0b2c80bab025dc1b29e94b
David Whitmore <whitters1793@googlemail.com>
2015-12-31 17:19:48 +0000

    Adds better javadoc for the JaxbContextProvider class.

--- src/main/java/org/dw/decision_variable_expander/JaxbContextProvider.java
+++ src/main/java/org/dw/decision_variable_expander/JaxbContextProvider.java
@@ -5,6 +5,10 @@ import javax.xml.bind.JAXBException;
 
 import com.google.inject.Provider;
 
+/**
+ * Used by Guice to create JAXBContexts as necessary. See also the configuration
+ * in the Module class.
+ */
 class JaxbContextProvider implements Provider<JAXBContext> {
     @Override
     public JAXBContext get() {
