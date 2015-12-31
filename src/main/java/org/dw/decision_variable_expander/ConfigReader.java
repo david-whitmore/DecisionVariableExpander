ba199815d92ce3b97cc521827285b57ef959b6cc
David Whitmore <whitters1793@googlemail.com>
2015-12-31 17:09:26 +0000

    Adds better javadoc for ConfigReader.

--- src/main/java/org/dw/decision_variable_expander/ConfigReader.java
+++ src/main/java/org/dw/decision_variable_expander/ConfigReader.java
@@ -6,6 +6,11 @@ import javax.inject.Inject;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+/**
+ * Reads configuration from an input stream, and returns a corresponding
+ * DecisionTable. Uses JAXB behind the scenes. See example_config/example1.xml
+ * etc for examples of the xml format.
+ */
 class ConfigReader {
     private final Unmarshaller unmarshaller;
 
