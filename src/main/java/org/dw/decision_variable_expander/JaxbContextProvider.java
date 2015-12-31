b7df81c135d52d2329916face485039793fe9103
David Whitmore <whitters1793@googlemail.com>
2015-12-31 16:25:33 +0000

    Fixed bug with JaxbContextProvider.

--- src/main/java/org/dw/decision_variable_expander/JaxbContextProvider.java
+++ src/main/java/org/dw/decision_variable_expander/JaxbContextProvider.java
@@ -9,7 +9,7 @@ class JaxbContextProvider implements Provider<JAXBContext> {
     @Override
     public JAXBContext get() {
         try {
-            return JAXBContext.newInstance(ConfigReader.class);
+            return JAXBContext.newInstance(DecisionTable.class);
         } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
